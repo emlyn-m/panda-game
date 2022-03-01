9385b59189ff0d38521e25e1150ab50e1558fc52
Emlyn Matheson <emlynmatheson@gmail.com>
2022-03-01 15:01:28 +1300

    Update __str__ method of Vector2

--- source/Vector2.py
+++ source/Vector2.py
@@ -6,9 +6,12 @@ class Vector2():
     """
     2D Vector object.
 
-    :param __values: float[2+] representing values of vector
-    :param __magnitude: float representing length of vector
-    :param __angle: float representing angle of vector from +x axis (radians)
+    :param __values: Values of vector
+    :type __values: float[2]
+    :param __magnitude: Length of vector
+    :type __magnitude: float
+    :param __angle: Angle of vector from +x axis (radians)
+    :type __angle: float
     """
 
     def __init__(self, valuesInit=[0, 0]):
@@ -16,6 +19,7 @@ class Vector2():
         Initialize.
 
         :param valuesInit: Initial values for vector
+        :type valuesInit: float[2]
         """
         self.__values = valuesInit
         self.__magnitude = self.__calculateMagnitude()
@@ -29,7 +33,12 @@ class Vector2():
 
     # Magic methods
     def __add__(self, other):
-        """Add another Vector2."""
+        """Add another Vector2.
+
+        :type other: Vector2
+        :rtype: Vector2
+        :raises ValueError: If other is not a Vector2
+        """
         if (type(other) == type(self)):
             return Vector2(
                 [x+y for x, y in zip(self.__values, other.__values)])
@@ -37,19 +46,47 @@ class Vector2():
             raise TypeError("Cannot add vector to scalar")
 
     def __sub__(self, other):
-        """Subtract another Vector2."""
+        """Subtract another Vector2.
+
+        :type other: Vector2
+        :rtype: Vector2
+        :raises ValueError: If other is not a Vector2
+        """
         self.__add__(self.__mul__(-1))
 
     def __mul__(self, other):
-        """Multiply by scalar."""
+        """
+        Multiply by scalar.
+
+        :type other: Scalar
+        :rtype: Vector2
+        :raises ValueError: If other is not a scalar
+        """
         try:
             int(other)  # Check numeric
             return Vector2([x * other for x in self.__values])
         except ValueError:
             raise TypeError("Cannot multiply vector")
 
+    def __div__(self, other):
+        """Divide by scalar.
+
+        :type other: Scalar
+        :rtype: Vector2
+        :raises ValueError: If other is not a scalar
+        """
+        try:
+            int(other)
+            return self.__mul__(1/other)
+        except ValueError:
+            raise TypeError("Cannot divide vector")
+
     def __str__(self):
         """Represent as String."""
+        return "(" + ",".join(map(str, self.__values)) + ")"
+
+    def __list__(self):
+        """Represent as List."""
         return self.__values
 
     # Accessors
