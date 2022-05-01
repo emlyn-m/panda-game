1ea4717648029127969052ef975d5fbaa8e02528
Emlyn Matheson <emlynmatheson@gmail.com>
2022-05-02 11:31:12 +1200

    add hazard code

--- source/Hazard.py
+++ source/Hazard.py
@@ -1,4 +1,15 @@
 from EnvObject import EnvironmentalObject
+from CollidablePhysicsManager import CollidablePhysicsManager
+
+class HazardPhysicsManager(CollidablePhysicsManager):
+
+    def collide(self, otherPhysicsManager):
+        cv = super().collide(otherPhysicsManager)
+        if list(cv) != [0, 0]:
+            # Move to dead zone
+
 
 class Hazard(EnvironmentalObject):
-    pass
+    def __init__(self, framesPath, fpf, size, posVec):
+        super().__init__(framesPath, fpf, size, posV)
+        self.physicsManager = HazardPhysicsManager(mass, sizeXY, mu, posVec, pygame.mask.Mask(size, fill=True))
